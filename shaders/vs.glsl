bee097719e17273d759fc5000994a04677bb08a3
Alessandro Vannoni <alevanno@live.it>
2020-09-11 02:31:30 +0200

    Add some lights

--- shaders/vs.glsl
+++ shaders/vs.glsl
@@ -17,4 +17,31 @@ void main() {
 	fsUVs = inUVs;
 	gl_Position = wvpMatrix * vec4(inPosition, 1.0);
 }
-	
+
+
+
+
+
+
+/*
+attribute vec3 inPosition; 
+attribute vec3 inNormal; 
+attribute vec2 inUVs;
+
+varying vec3 fsNormal; 
+varying vec4 fsPosition; 
+varying vec2 fsUVs;
+
+uniform mat4 wvpMatrix;  //WorldViewProjection Matrix
+uniform mat4 worldViewMatrix; //WorldView Matrix
+//For camera space:
+//Matrix to apply transformation on normals
+uniform mat4 nMatrix;
+
+void main() { 
+	fsNormal = mat3(nMatrix) * inNormal; 
+	fsPosition =  worldViewMatrix * vec4(inPosition,1.0); //coordinates in Camera Space
+	fsUVs = inUVs;
+
+	gl_Position = wvpMatrix * vec4(inPosition, 1.0);
+}*/
